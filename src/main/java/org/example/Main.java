b55df66014bfb670866130606b640e519131d977
Abbes <larbaouiabbes@gmail.com>
2022-11-19 18:18:04 +0100

    facade design pattern

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.shapes.ShapeMaker;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
---
+++ src/main/java/org/example/shapes/Rectangle.java
@@ -0,0 +1,9 @@
+package org.example.shapes;
+
+public class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Rectangle::draw()");
+    }
+}
